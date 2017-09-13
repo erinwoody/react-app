17b48bbbbf16a8b3a6dc587d9f462a484409b33d
Erin Woody <erinelizabethwoody@gmail.com>
2017-09-13 15:47:27 -0500

    added earthquake lists and info

--- components/EarthquakeList.js
+++ components/EarthquakeList.js
@@ -5,42 +5,40 @@ import earthquakes from "../data/earthquakes";
 
 export default class EarthquakeList extends Component {
   render() {
-    let earthquakeFeatures = this.features.map(earthquakes, index) => (
-        
-    );
-
-
-
-    return (
-        <div className="col-sm-6" key={earthquakes.features.id}>
+    let quakes = earthquakes.features.map((item, index) => {
+    
+      return (
+        <div className="col-sm-6" key={item.id}>
           <div className="card">
             <div className="card-block">
-              <h4 className="card-title">{earthquakes.features.place}</h4>
+              <h4 className="card-title">{item.properties.place}</h4>
               <h6 className="card-subtitle mb-2 text-muted">
-                Magnitude: {earthquakes.features.mag}
+                Magnitude: {item.properties.mag}
               </h6>
               <h6 className="card-subtitle mb-2 text-muted">
-                Time: {moment(earthquakes.features.time).format("llll")}
+                Time: {moment(item.properties.time).format("llll")}
               </h6>
               <p className="card-text">
-                Coordinates: {earthquakes.features.coordinates}
+                Coordinates: {item.geometry.coordinates}
               </p>
 
-              <a href={earthquakes.features.url} className="card-link">
+              <a href={item.properties.url} className="card-link">
                 USGS Event Link
-              </a>
+                </a>
             </div>
+          </div>
         </div>
+      );
+    })
+
+    return (
+      <div className="quake-list">
+
+        <div className="row">
+          {quakes}
+        </div>
+
       </div>
-    );    
-  }
+    );
+  }   
 }
-return (
-<div className="quake-list">
-
-  <div className="row">
-    {quakes}
-  </div>
-
-  </div>
-)
